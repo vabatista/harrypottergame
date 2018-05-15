c1a93e6a1cf8f8cc33de9f44e3bce29971450d8f
Leonardo Carreiro <leonardo.mcarreiro@gmail.com>
2018-05-15 20:33:22 -0300

    Incluindo o atributo 'name' no input das opções de múltipla escolha, para permitir apenas uma seleção

--- angular-app/src/app/pergunta/pergunta-multipla-escolha.component.ts
+++ angular-app/src/app/pergunta/pergunta-multipla-escolha.component.ts
@@ -7,7 +7,7 @@ import { PerguntaMultiplaEscolha } from './pergunta.component';
     <div class="form-group">
       <label for="pergunta-multipla-escolha">{{pergunta.texto}}</label>
       <div class="form-check" *ngFor="let opcao of pergunta.opcoes">
-        <input class="form-check-input" type="radio" id="opcao-{{opcao}}" value="opcao">
+        <input class="form-check-input" type="radio" name="resposta" id="opcao-{{opcao}}" value="opcao">
         <label class="form-check-label" for="opcao-{{opcao}}">
           {{opcao}}
         </label>
